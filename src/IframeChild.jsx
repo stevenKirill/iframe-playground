f3dbf7c3c0e285a0f8b60c14844a1854440ce7de
Кирилл Павловский <kirill-pavlovskij@rambler.ru>
2021-09-05 23:15:41 +0300

    initial

---
+++ src/IframeChild.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function IframeChild() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default IframeChild
---
+++ src/IframeParent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function IframeParent() {
+    return (
+        <div>
+            
+        </div>
+    )
+};
+
+export default IframeParent;
